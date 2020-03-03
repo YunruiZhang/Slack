60bbac64c64bf97bb7209627ca73c584884d3105
Edward <yunrui.zhang@student.unsw.edu.au>
2020-03-03 23:50:11 +1100

    finish register tests

--- src/auth_test.py
+++ src/auth_test.py
@@ -19,12 +19,36 @@ def test_auth_logout():
 	# Given an active token, invalidates the taken to log the user out. If a valid token is given, and the user is successfully logged out, it returns true, otherwise false.
 
 def test_auth_register():
+	result = auth.auth_register('yunrui.zhang@studnet.unsw.edu.au','123456','Yunrui','Zhang')
 	# Function auth_register(email,password,name_first,name_last)
 	# Returns {u_id,token}
 	# Given a user's first and last name, email address, and password, create a new account for them and return a new token for authentication in their session. A handle is generated that is the concatentation of a lowercase-only first name and last name. If the concatenation is longer than 20 characters, it is cutoff at 20 characters. If the handle is already taken, you may modify the handle in any way you see fit to make it unique.
 
+
+
 def test_auth_refister_except():
-	# InputError:
+	#short password
+	with pytest.raises(InputError) as e:
+		auth.auth_register('yunrui.zhang@studnet.unsw.edu.au','12345','Yunrui','Zhang')
+	#Invalid email
+	with pytest.raises(InputError) as e:
+		auth.auth_register('i hate python','12345','Yunrui','Zhang')
+	#double register
+	result = auth.auth_register('yunrui.zhang1@studnet.unsw.edu.au','123456','Yunrui','Zhang')
+	with pytest.raises(InputError) as e:
+		result1 = auth.auth_register('yunrui.zhang1@studnet.unsw.edu.au','123456','Yunrui','Zhang')
+	#short first name
+	with pytest.raises(InputError) as e:
+		result2 = auth.auth_register('yunrui.zhang2@studnet.unsw.edu.au','123456','','Zhang')
+	#long first name
+	with pytest.raises(InputError) as e:
+		result2 = auth.auth_register('yunrui.zhang3@studnet.unsw.edu.au','123456','y*50','Zhang')
+	#short last name
+	with pytest.raises(InputError) as e:
+		result2 = auth.auth_register('yunrui.zhang4@studnet.unsw.edu.au','123456','Yunrui','')
+	#long last name
+	with pytest.raises(InputError) as e:
+		result2 = auth.auth_register('yunrui.zhang5@studnet.unsw.edu.au','123456','Yunrui','Z*50')
 	#	Email entered is not a valid email 
 	#	Email address is already being used by another user
 	#	Password entered is less than 6 characters long
