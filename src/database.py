944b8fcd7e8128368984512ca89dca6d62bcd524
Edward <yunrui.zhang@student.unsw.edu.au>
2020-03-24 14:12:24 +1100

    prototype of databse

---
+++ src/database.py
@@ -0,0 +1,13 @@
+# user is a list of dictionarys contain user info. there are uid, email, password in the dictioary
+# tokens is a list of token which is valid
+# channels is a list of dicts contain existing channels info.
+# msgs is a list of dicts contain msg infos including the channel id which the msg in 
+# and the sender of it , the time it sent and the msg itself.
+
+
+DATABASE = {
+    user = []
+    tokens = []
+    channels = []
+    msgs = []
+}
