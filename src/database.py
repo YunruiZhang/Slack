5e8a4ac31ca06df19ebc6ed2006f11973f3518a7
Chris Chung <z5162686@ad.unsw.edu.au>
2020-03-24 23:05:27 +0800

    Added channels database list structure

--- src/database.py
+++ src/database.py
@@ -1,9 +1,38 @@
 # user is a list of dictionarys contain user info. there are uid, email, password in the dictioary
 # tokens is a list of token which is valid
-# channels is a list of dicts contain existing channels info.
+# channels is a list of dicts contain existing channels info.   
 # msgs is a list of dicts contain msg infos including the channel id which the msg in 
 # and the sender of it , the time it sent and the msg itself.
 import jwt
+
+'''
+EXAMPLE OF HOW THE CHANNELS DATABASE STRUCTURE LOOKS:
+
+"channels" : [
+        {
+            "channel_id":1,
+            "public":1,
+            "details" : {
+            "name": "example name",
+            "owner_members": [
+                {
+                    "u_id": 1,
+                    "name_first": "example first",
+                    "name_last": "example last",
+                }
+            ],
+            "all_members": [
+                {
+                    "u_id": 1,
+                    "name_first": "example first",
+                    "name_last": "example last",
+                }
+            ],
+            },
+            "messages":[{"ADD THE MESSAGE DICTIONARY TO THIS LIST PLEASE"}]
+        }
+    ]
+'''
 
 DATABASE = {
     'users' : [],
