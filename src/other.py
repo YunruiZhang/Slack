707915e3456f8758a9a71834abb9307616768dbe
GUAN YINGDI <z5235418@ad.unsw.edu.au>
2020-03-26 15:56:00 +1100

    finish ver1 for other.py

--- src/other.py
+++ src/other.py
@@ -2,7 +2,13 @@ def users_all(token):
     all_user = []
     Data = getdata()
     
-   return(all_user)
+    operate_u_id = verify_token(token)
+    if not operate_u_id:
+        raise AccessError('Token Invalid')
+    for user in Data['users']:
+        all_user.append(user)
+    
+    return{ 'list' : all_user }
    
 ''' return {
         'users': [
@@ -17,6 +23,25 @@ def users_all(token):
     }'''
 
 def search(token, query_str):
+    search_message = []
+    Data = getdata()
+    
+    for single_message in Data['messages']:
+        if query_str in single_message['message']:
+            i = 0
+            if search_message = []:
+                search_message[0] = single_message
+            else:
+                while(i < len(single_message)):
+                    time_origin = search_message[i]['time_created']
+                    if single_message['time_created'] > time_origin:
+                        search_message[1] = search_message[0]
+                        search_message[0] = single_message
+                        break
+                    else:
+                        i = i + 1
+    
+    return { 'messages' : search_message }
     
     
     '''return {
