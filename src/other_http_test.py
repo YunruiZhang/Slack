53484048ae8d33f480d76d2d1d68b4837709894d
Yingdi Guan <yingdi.guan@student.unsw.edu.au>
2020-03-29 19:23:20 +1100

    Update other_http_test.py

--- src/other_http_test.py
+++ src/other_http_test.py
@@ -2,27 +2,21 @@ import urllib
 import json
 from flask import request
 import pytest
-BASE_URL = ''
+BASE_URL = 'http://127.0.0.1:8081'
 
 def test_users_all():
     register_person()
     person1 = login_person()
     person1_token = person1['token']
     
-    data = json.dumps({
-        'token': person1_token
-    })ecode('utf-8')
-    req = urllib.request.Request(f"{BASE_URL}/other/users_all",
-                                 data=data,
-                                 headers={'Content-Type': 'application/json'})
-    response = urllib.request.urlopen(req)
+    response = urllib.request.urlopen(f'{BASE_URL}/other/users_all?token={person1_token}')
     return_person = json.load(response)
     
-    assert(return_person['email'] == 'cs1531@cse.unsw.edu.au')
-    assert(return_person['password'] == 'abc123')
-    assert(return_person['name_first'] == 'Hayden')
-    assert(return_person['name_last'] == 'Jacobs')
-    assert(return_person['handle_str'] == 'hjacobs')
+    assert(return_person[0]['email'] == 'cs1531@cse.unsw.edu.au')
+    assert(return_person[0]['password'] == 'abc123')
+    assert(return_person[0]['name_first'] == 'Hayden')
+    assert(return_person[0]['name_last'] == 'Jacobs')
+    assert(return_person[0]['handle_str'] == 'hjacobs')
     
 def test_search():
     register_person()
@@ -32,7 +26,7 @@ def test_search():
         'token': person1_token,
         'name' : 'Channel 1',
         'is_public' : 'True'
-    })ecode('utf-8')
+    }).encode('utf-8')
     req = urllib.request.Request(f"{BASE_URL}/channels/create",
                                  data=data,
                                  headers={'Content-Type': 'application/json'})
@@ -43,21 +37,15 @@ def test_search():
         'token': person1_token,
         'channel_id' : new_channel,
         'message' : 'Hello world'
-    })ecode('utf-8')
+    }).encode('utf-8')
     req = urllib.request.Request(f"{BASE_URL}/message/send",
                                  data=data,
                                  headers={'Content-Type': 'application/json'})
     response = urllib.request.urlopen(req)
     message1_id = json.load(response)['message_id']
     
-    data = json.dumps({
-        'token': person1_token,
-        'query_str' = 'Hello'
-    })ecode('utf-8')
-    req = urllib.request.Request(f"{BASE_URL}/other/search",
-                                 data=data,
-                                 headers={'Content-Type': 'application/json'})
-    response = urllib.request.urlopen(req)
+    
+    response = urllib.request.urlopen(f'{BASE_URL}/other/search?token={person1_token}&query_str={'Hello'}')
     message1_collection = json.load(response)
     
     assert(message1_collection[0]['message_id'] = message1_id)
@@ -68,14 +56,14 @@ def test_search():
     
 def register_person():
     data = json.dumps({
-    	'email': 'cs1531@cse.unsw.edu.au'
-    	'password': 'abc123'
-    	'name_first': 'Hayden'
+    	'email': 'cs1531@cse.unsw.edu.au',
+    	'password': 'abc123',
+    	'name_first': 'Hayden',
     	'name_last': 'Jacobs'
     	}).encode('utf-8')
     req = urllib.request.Request(f"{BASE_URL}/auth/register",
                                  data=data,
-                                 headers={'Content-Type': 'application/json'},
+                                 headers={'Content-Type': 'application/json'}
                                  )
     response = urllib.request.urlopen(req)
     person = json.load(response)
@@ -83,14 +71,14 @@ def register_person():
 
 def register_person_second():
     data = json.dumps({
-    	'email': 'cs1532@cse.unsw.edu.au'
-    	'password': 'abc1234'
-    	'name_first': 'Hden'
+    	'email': 'cs1532@cse.unsw.edu.au',
+    	'password': 'abc1234',
+    	'name_first': 'Hden',
     	'name_last': 'Marry'
     	}).encode('utf-8')
     req = urllib.request.Request(f"{BASE_URL}/auth/register",
                                  data=data,
-                                 headers={'Content-Type': 'application/json'},
+                                 headers={'Content-Type': 'application/json'}
                                  )
     response = urllib.request.urlopen(req)
     person = json.load(response)
@@ -98,15 +86,14 @@ def register_person_second():
     
 def login_person():
     data = json.dumps({
-    	'email': 'cs1531@cse.unsw.edu.au'
+    	'email': 'cs1531@cse.unsw.edu.au',
     	'password': 'abc123'
     	}).encode('utf-8')
     req = urllib.request.Request(f"{BASE_URL}/auth/login",
                                  data=data,
-                                 headers={'Content-Type': 'application/json'},
+                                 headers={'Content-Type': 'application/json'}
                                  )
     response = urllib.request.urlopen(req)
     login_person = json.load(response)
     return login_person
     
-
