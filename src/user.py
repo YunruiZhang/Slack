b865f168f48f526f1c9f6ff4e55452cc9056e14e
Yingdi Guan <yingdi.guan@student.unsw.edu.au>
2020-03-29 17:03:39 +1100

    Update user.py

--- src/user.py
+++ src/user.py
@@ -18,21 +18,27 @@ def user_profile(token, u_id):
 
     if not foundFlag:
         raise InputError('Invalid User ID')
+    return_user = {}
+    return_user['u_id'] = users['u_id']
+    return_user['email'] = users['email']
+    return_user['name_first'] = users['name_first']
+    return_user['name_last'] = users['name_last']
+    return_user['handle'] = users['handle']
     
-    return {'user':users}
+    return {'user':return_user}
     '''
     return {'user': {
         	'u_id': 1,
         	'email': 'cs1531@cse.unsw.edu.au',
         	'name_first': 'Hayden',
         	'name_last': 'Jacobs',
-        	'handle_str': 'hjacobs',
+        	'handle': 'hjacobs',
         }
     }
     '''
 
 def user_profile_setname(token, name_first, name_last):
-    DATA = getData()
+    Data = getData()
 
     
     if not user_name_length_check(name_first):
@@ -40,14 +46,14 @@ def user_profile_setname(token, name_first, name_last):
         
     if not user_name_length_check(name_last):
         raise InputError('Length of last name is invalid') 
-    
-    operate_u_id = verify_token(token)
-    if not operate_u_id:
-        raise AccessError('Token Invalid')
-    
+    if not verify_token(token):
+        operate_u_id = verify_token(token)
+   # if not operate_u_id:
+      #  raise AccessError('Token Invalid')
+    curr_u_id = verify_token(token)
     user = None
     for user in Data['users']:
-        if int(user['u_id']) == operate_u_id:
+        if int(user['u_id']) == curr_u_id:
             break   
     
     user['name_first'] = name_first
@@ -57,21 +63,21 @@ def user_profile_setname(token, name_first, name_last):
     }
 
 def user_profile_setemail(token, email):
-    DATA = getData()
+    Data = getData()
     
-    if not user_email_check(email,token):
+    if not user_email_check(email):
         raise InputError('Email has already been used')
     
     if not check(email):
         raise InputError("Email entered is not a valid email address")
         
     curr_u_id = verify_token(token)
-    if not curr_u_id:
-        raise AccessError('Token Invalid')
+   # if not curr_u_id:
+    #    raise AccessError('Token Invalid')
     
     user = None
     for user in Data['users']:
-        if int(user['u_id']) == operate_u_id:
+        if int(user['u_id']) == curr_u_id:
             break   
     
     user['email'] = email   
@@ -82,19 +88,19 @@ def user_profile_setemail(token, email):
 def user_profile_sethandle(token, handle_str):
     DATA = getData()
     
-    if not user_handle_length_check(handle):
+    if not user_handle_length_check(handle_str):
         raise InputError('Length of handle is invalid')
         
-    if not user_handle_check(email,token):
+    if not user_handle_check(handle_str):
         raise InputError('Handle has already been used')
     
     curr_u_id = verify_token(token)
-    if not curr_u_id:
-        raise AccessError('Token Invalid')
+   # if not curr_u_id:
+   #     raise AccessError('Token Invalid')
        
     user = None
-    for user in Data['users']:
-        if int(user['u_id']) == operate_u_id:
+    for user in DATA['users']:
+        if int(user['u_id']) == curr_u_id:
             break   
     
     user['handle'] = handle_str
@@ -112,16 +118,16 @@ def user_profile_sethandle(token, handle_str):
 
 
 
-def user_email_check(email,token):
+def user_email_check(email):
     DATA = getData()
-    for users in Data['users']:
+    for users in DATA['users']:
         if users['email'] == email:
             return False     
     return True
 
 def user_handle_check(handle):
     DATA = getData()
-    for users in Data['users']:
+    for users in DATA['users']:
         if users['handle'] == handle:
             return False     
     return True
@@ -148,5 +154,4 @@ def check(email):
           
     else:  
         return False 
-
 
